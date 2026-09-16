9c5972781db9579cabe699779c996fb89b956870
DangKhoa <43160084+KhoaDang-Dinh@users.noreply.github.com>
2026-09-16 17:12:25 +0700

    Add filter for opt-in status in availability zones
    Added a filter to the aws_availability_zones data source to specify the opt-in status.

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,5 +1,10 @@
 data "aws_availability_zones" "available" {
   state = "available"
+
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
 }
 
 data "aws_caller_identity" "current" {}

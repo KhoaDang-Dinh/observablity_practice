e7c0399d495284ba0e7bbc2b057c54dda50074f3
DangKhoa <43160084+KhoaDang-Dinh@users.noreply.github.com>
2026-09-17 14:38:09 +0700

    infra: fix S3 encryption config

--- infra/terraform/telemetry_s3.tf
+++ infra/terraform/telemetry_s3.tf
@@ -37,8 +37,6 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "telemetry" {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
-
-    bucket_key_enabled = true
   }
 }
 
